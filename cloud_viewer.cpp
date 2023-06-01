bc8efd2f69ebd43db2c33e169b8ec24c2cd1bfcf
Yuki <miyake_yuuki320@yahoo.co.jp>
2023-06-01 21:30:22 +0900

    upsampling

--- cloud_viewer.cpp
+++ cloud_viewer.cpp
@@ -1,29 +1,25 @@
 #include <pcl/visualization/cloud_viewer.h>
-#include <iostream>
-#include <thread>
+// #include <iostream>
+// #include <thread>
 
 #include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
+// #include <pcl/point_types.h>
 
-
-
-
-int
-main (int argc, char** argv)
+int main(int argc, char **argv)
 {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *cloud) == -1)
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  if (pcl::io::loadPCDFile<pcl::PointXYZ>(argv[1], *cloud) == -1)
   {
-    PCL_ERROR ("Couldn't read file \n");
+    PCL_ERROR("Couldn't read file \n");
     return (-1);
   }
-  std::cout << "Loaded " << cloud->size () << " data points" << std::endl;
+  std::cout << "Loaded " << cloud->size() << " data points" << std::endl;
 
-    pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-    viewer.showCloud (cloud);
-    while (!viewer.wasStopped ())
-    {
-    }
+  pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+  viewer.showCloud(cloud);
+  while (!viewer.wasStopped())
+  {
+  }
 
   return (0);
 }
